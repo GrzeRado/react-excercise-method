8cb9959cd8b46d6b8b1049885842b2000c43e96d
Grzegorz <rc36@wp.pl>
2018-01-19 10:35:32 +0100

    FirebaseToDo - List, deleteTask and this.deleteTask

--- src/FirebaseToDo.js
+++ src/FirebaseToDo.js
@@ -2,30 +2,43 @@ import React from 'react'
 import TextField from 'material-ui/TextField'
 import Paper from 'material-ui/Paper'
 import RaisedButton from 'material-ui/RaisedButton'
+import {List, ListItem} from 'material-ui/List';
+import ActionDelete from 'material-ui/svg-icons/action/delete';
 
 
 const styles = {
     margin: 20,
     padding: 20,
     textAlign: 'center'
-
-
 }
 
 class FirebaseToDo extends React.Component {
-    render(){
-        return(
+    deleteTask = (taskId) => {
+        alert(taskId)
+    }
+
+    render() {
+        return (
             <Paper style={styles}>
                 <TextField
                     hintText={"Nowe zadanie"}
+                    fullWidth={true}
                 />
                 <RaisedButton
                     label={"Dodaj!"}
+                    primary={true}
+                    fullWidth={true}
                 />
+                <List style={{textAlign: 'left'}}>
+                    <ListItem
+                        primaryText="Pierwsze zadanie!"
+                        rightIcon={<ActionDelete onClick={this.deleteTask('0')}/>}
+                    />
+                </List>
             </Paper>
         )
     }
 }
 
 
-export default  FirebaseToDo
+    export default  FirebaseToDo
