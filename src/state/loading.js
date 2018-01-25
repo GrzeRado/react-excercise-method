8108acf4d833a28452c2c9098506d82a8d3db31e
Grzegorz <rc36@wp.pl>
2018-01-25 10:43:00 +0100

    add loading.js

--- src/state/loading.js
+++ src/state/loading.js
@@ -10,3 +10,24 @@ export const startLoading = () => ({
 export const stopLoading = () => ({
     type: STOP
 })
+
+const initialState = {
+    isLoading: false
+}
+
+export default (state = initialState, action) => {
+    switch (action.type) {
+        case START:
+            return {
+                ...state,
+                isLoading: true
+            }
+        case STOP:
+            return {
+                ...state,
+                isLoading: false
+            }
+        default:
+            return state
+    }
+}
