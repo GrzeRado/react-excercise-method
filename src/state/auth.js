ecc2dc4eb97fb0ee5dcd07db55c4e7a469dc47dc
Grzegorz <rc36@wp.pl>
2018-01-26 10:54:32 +0100

    auth.js added info about login user

--- src/state/auth.js
+++ src/state/auth.js
@@ -1,5 +1,5 @@
 
-import {auth, googleProvider} from '../firebase'
+import {database, auth, googleProvider} from '../firebase'
 
 const SET_USER = 'auth/SET_USER'
 
@@ -14,8 +14,21 @@ export const initAuth = () => (dispatch, getState) => {
     auth.onAuthStateChanged((user) => {
         // if not logged in user is null !
         dispatch(setUser(user))
+
+        if(user) { //check if not null
+            dispatch(logLoginDate())
+        }
     })
 }
+
+const logLoginDate = () => (dispatch, getState) => { //Function uid comes from store.js and change state
+    const uid = getState().auth.user.id
+    database.ref(`/users/${uid}/loginLogs`)
+        .push({timestamp: Date.now()})
+        .then(() => console.log('Login date succesfully logged in db!'))
+        .catch(() =>  console.log('Something wrong!'))
+}
+
 
 export const logIn = (email, password) => (dispatch, getState) => {
     auth.signInWithEmailAndPassword(email, password)
