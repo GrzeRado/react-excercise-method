dd459a727044ed1c37bdaf48b0e68724b57521cd
Grzegorz <rc36@wp.pl>
2018-01-29 09:30:29 +0100

    add test to App.test

--- src/state/App.test.js
+++ src/state/App.test.js
@@ -3,3 +3,10 @@ const add = (a, b) => a + b
 test('it can and 2 + 5 and equals 7', () => {
     expect(add(2,5)).toBe(7)
 })
+
+
+test('it can add 10 + 5 and equals 15', () => {
+    expect(add('10', '5')).toBe(15)
+}
+
+//--------------------------------------
